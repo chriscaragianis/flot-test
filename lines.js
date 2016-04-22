ac09a60e6cf6b09411231fe43a622a6cda680050
Chris Caragianis <chriscaragianis@gmail.com>
2016-04-22 14:18:38 -0400

    xaxis looking good

--- lines.js
+++ lines.js
@@ -1,19 +1,30 @@
-var forecast;
+var plot_data = function(forecast) {
+  lines = [];
+  for (var acct in forecast.accounts) {
+     var dataObject = {};
+     dataObject.label = acct;
+     dataObject.data = forecast.accounts[acct].balances;
+     lines.push(dataObject);
+  }
+  return lines;
+}
+
+var xaxis_data = function(forecast) {
+  var dataObject = {
+    mode: "time",
+    ticklength: 0,
+    timeformat: "%m/%d/%y"
+  }  
+  return dataObject;
+}
 
 var data = $.ajax( {
   url: 'data.json',
   success: function(resp) {
-    forecast = resp;
-    lines = [];
-    for (var acct in forecast.accounts) {
-       var dataObject = {};
-       dataObject.label = acct;
-       dataObject.data = forecast.accounts[acct].balances;
-       lines.push(dataObject);
-    }
-    $.plot("#placeholder", lines);
+    $.plot("#placeholder", plot_data(resp), {
+        xaxis: xaxis_data(resp)
+    });
     $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
   }
 });
-    // Add the Flot version string to the footer
-
+    
